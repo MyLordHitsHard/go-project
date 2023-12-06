06279d84e9f803363e3cee02fb2ef3f5ad691bfa
Himanshu Dutt <37894863+MyLordHitsHard@users.noreply.github.com>
2023-12-06 17:49:12 +0530

    Connecting MySQL Database with Golang

--- main.go
+++ main.go
@@ -2,13 +2,43 @@ package main
 
 import "gofr.dev/pkg/gofr"
 
+type Customer struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
 func main() {
 	app := gofr.New()
 
 	app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
-		value, err := ctx.Redis.Get(ctx.Context, "greeting").Result()
 
-		return value, err
+		return "Hello World! Again!!!", nil
+	})
+
+	app.POST("/customer/{name}", func(ctx *gofr.Context) (interface{}, error) {
+		name := ctx.PathParam("name")
+
+		_, err := ctx.DB().ExecContext(ctx, "INSERT INTO customers (name) VALUES (?)", name)
+
+		return nil, err
+	})
+
+	app.GET("/customer", func(ctx *gofr.Context) (interface{}, error) {
+		var customers []Customer
+		rows, err := ctx.DB().QueryContext(ctx, "SELECT * FROM customers")
+		if err != nil {
+			return nil, err
+		}
+
+		for rows.Next() {
+			var customer Customer
+			if err := rows.Scan(&customer.ID, &customer.Name); err != nil {
+				return nil, err
+			}
+			customers = append(customers, customer)
+		}
+
+		return customers, nil
 	})
 
 	app.Start()
