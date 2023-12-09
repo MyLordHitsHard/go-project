e153b45f79a66344785b071794212769339044d6
Himanshu Dutt <37894863+MyLordHitsHard@users.noreply.github.com>
2023-12-09 07:45:16 +0530

    Added the POST request endpoint

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -2,23 +2,23 @@ package datastore
 
 import (
 	"database/sql"
-	"gofr.dev/pkg/gofr"
 	"gofr.dev/pkg/errors"
+	"gofr.dev/pkg/gofr"
 	"go-project/model"
 )
 
-type carInfo struct {}
+type carInfo struct{}
 
 func New() *carInfo {
 	return &carInfo{}
 }
 
-func (c *carInfo) GetByID (ctx *gofr.Context, id string) (*model.CarGarage, error) {
+func (c *carInfo) GetByID(ctx *gofr.Context, id string) (*model.CarGarage, error) {
 	var response model.CarGarage
 
 	err := ctx.DB().QueryRowContext(ctx, "SELECT * FROM car-garage WHERE id=$1", id).
-		Scan(&response.ID, &response.LicensePlate, &response.Make, &response.Model, &response.Color, &response.EntryTime, &response.RepairStatus)
-	
+		Scan(&response.ID, &response.license_plate, &response.make, &response.model, &response.color, &response.entry_time, &response.repair_status)
+
 	switch err {
 	case sql.ErrNoRows:
 		return &model.CarGarage{}, errors.EntityNotFound{Entity: "car", ID: id}
@@ -27,4 +27,18 @@ func (c *carInfo) GetByID (ctx *gofr.Context, id string) (*model.CarGarage, erro
 	default:
 		return &model.CarGarage{}, err
 	}
+}
+
+func (c *carInfo) Create(ctx *gofr.Context, carInfo *model.CarGarage) (*model.CarGarage, error) {
+	var response model.CarGarage
+	err := ctx.DB().QueryRowContext(ctx, "INSERT INTO car-garage (license_plate, make, model, color, entry_time, repair_status, log_id, log_time) VALUES($1, $2, $3, $4, $5, $6, $7, $8)"+
+		"RETURNING id, license_plate, make, model, color, entry_time, repair_status, log_id, log_time", carInfo.LicensePlate, carInfo.Make, carInfo.Model, carInfo.Color, carInfo.EntryTime, carInfo.RepairStatus, carInfo.LogID, carInfo.LogTime).Scan(
+		&response.ID, &response.license_plate, &response.make, &response.model, &response.color, &response.entry_time, &response.repair_status, &response.log_id, &response.log_time)
+
+	if err != nil {
+		return &model.CarGarage{}, err
+	}
+
+	return &response, nil
+
 }
